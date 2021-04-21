f70ca4bf3e3aabdcb677ca127d1d04a59f7f3070
LuisAlvarado-19 <luafer95@gmail.com>
2021-04-21 03:39:03 +0000

    actualizacion

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let si = document.getElementById('si');
 si.addEventListener("click", function (e) {
-    swal('Mañana nos casamos 😂', 'Sabia que que querias ser mi Novia. ❤️')
+    swal('Sabia que si querias ser mi Novia ❤️ aunque casi obligada 😂', 'Antes de irte has click en la 🌹')
 });
 let no = document.getElementById('no');
 no.addEventListener("mousemove", function (e) {
